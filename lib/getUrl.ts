7f588f4538553d36afe974352e7022dbfd443e7a
Raphael Luersen <raphaelluersen@yahoo.co.uk>
2023-12-13 07:39:19 +0000

    first commit

--- lib/getUrl.ts
+++ lib/getUrl.ts
@@ -1,4 +1,5 @@
 import { storage } from "@/appwrite";
+import { Image } from "@/typings";
 
 const getUrl = async (image: Image) => {
     const url= storage.getFilePreview(image.bucketId, image.fileId);
